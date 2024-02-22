45104882e3135160fea9f051abb8a7a8416ea003
Bartosz <szczesniak.bartosz@gmail.com>
2024-02-22 14:25:49 +0100

    builded llm+rag chain with simple io script

---
+++ llm_chain_with_rag.py
@@ -0,0 +1,83 @@
+import chromadb
+
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
+from langchain_community.llms.llamacpp import LlamaCpp
+
+from sentence_transformers import SentenceTransformer
+
+from utils.config import CONFIG
+from utils.classes import STCDBRetriever
+
+def build_chain():
+
+    # RAG collection
+
+    collection = chromadb.PersistentClient(
+        path=CONFIG['chromadb']["path"]
+    ).get_collection(
+        name=CONFIG["chromadb"]["collection"]
+    )
+
+    # LLM
+
+    llm = LlamaCpp(
+        model_path=CONFIG['llama']['path'],
+        temperature=0.75,
+        max_tokens=1024,
+        top_p=1,
+        n_ctx=1024, 
+        cache=True
+    )
+    llm.client.verbose = False
+
+    # RAG model / retriever
+
+    emb_model = SentenceTransformer(model_name_or_path=CONFIG['gist']['path'])
+    retriever = STCDBRetriever(model=emb_model, collection=collection, n_results=1)
+
+    # prompt template
+
+    llm_prompt_template = """
+        Generate the answer to the question using only given context and no prior knowledge.
+        Your answer should be as concise as possible.
+        If the context is empty, answer only that you have no information on this topic.
+        
+        Context: {context}
+        
+        Question: {question}
+        
+        Answer:
+    """
+
+    llm_prompt = PromptTemplate(
+        input_variables=['question', 'context'],
+        template=llm_prompt_template
+        )
+
+    # Output parser
+
+    parser = StrOutputParser()
+
+    # Med assist chain
+
+    return {"context": retriever, "question": RunnablePassthrough()} | llm_prompt | llm | parser
+
+def run_io_script(chain):
+
+    while True:
+        
+        question = input("\nQuestion: ")
+
+        print("Answer: ", end="", flush=True)
+
+        for s in chain.stream(question):
+            print(s, end="", flush=True)
+
+if __name__ == "__main__":
+
+    chain = build_chain()
+
+    run_io_script(chain)
---
+++ utils/classes.py
@@ -0,0 +1,32 @@
+from typing import List
+from langchain_core.callbacks.manager import CallbackManagerForRetrieverRun
+from langchain_core.documents import Document
+from langchain_core.retrievers import BaseRetriever
+from sentence_transformers import SentenceTransformer
+from chromadb.api.models.Collection import Collection
+
+class STCDBRetriever(BaseRetriever):
+    model:SentenceTransformer
+    collection:Collection
+    n_results:int
+    
+    def _get_relevant_documents(self, query: str, *, run_manager: CallbackManagerForRetrieverRun) -> List[Document]:
+        
+        query_embedding = self._embed_query(query)
+        results = self._retrieve_from_collection(query_embedding)
+        documents = results['documents']
+
+        return documents
+    
+    def _embed_query(self, query):
+        return self.model. \
+            encode(query). \
+            tolist()
+    
+    def _retrieve_from_collection(self, query_embedding):
+
+        return self.collection.query(
+            query_embeddings=query_embedding,
+            n_results=self.n_results, 
+            include=['documents',]
+        )
