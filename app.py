d42674ac8593d6342ea0489c5468eebb157430e0
Bartosz <szczesniak.bartosz@gmail.com>
2024-02-23 11:21:04 +0100

    simple LangServe playground API

---
+++ app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from langserve import add_routes
+from llm_chain_with_rag import build_chain
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI(
+    title="med_assist",
+    version="0.1.0",
+    description="Medical assistant API"
+)
+
+chain = build_chain()
+
+add_routes(
+    app=app,
+    runnable=chain,
+    path="/med_assist"
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000, )
