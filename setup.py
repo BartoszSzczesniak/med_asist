f3d27e704047d6e2154575b1a1c844dab20e12f9
szczesniakbartosz <szczesniak.bartosz@gmail.com>
2024-05-02 11:02:51 +0000

    bind project functions into a package

--- setup.py
+++ setup.py
@@ -7,13 +7,15 @@ print(long_descr)
 
 setup(
     name="med_assist",
-    version="0.2.0",
+    version="0.3.0",
     description="Internal package for medical assistant project.",
     long_description=long_descr,
     long_description_content_type="text/markdown",
     author="Bartosz Szcześniak",
     author_email="szczesniak.bartosz@gmail.com",
     url="https://github.com/BartoszSzczesniak/med_assist",
-    packages=['med_assist',],
+    packages=['med_assist', 'med_assist.components'],
     package_dir={"med_assist": "med_assist"},
+    include_package_data=True,
+    package_data={"med_assist": ["data/*",]},
     )
